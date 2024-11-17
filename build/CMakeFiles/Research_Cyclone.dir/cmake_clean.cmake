59721dd6784b55dad4ad77a3848d110717294c28
Tanishq Dwivedi <tanishq12.dwivedi@gmail.com>
2024-11-17 14:09:48 -0800

    starting commit to move over to Visual Studio.

---
+++ build/CMakeFiles/Research_Cyclone.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Research_Cyclone.dir/main.cpp.o"
+  "CMakeFiles/Research_Cyclone.dir/main.cpp.o.d"
+  "Research_Cyclone"
+  "Research_Cyclone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Research_Cyclone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/Research_Cyclone.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Research_Cyclone.
+# This may be replaced when dependencies are built.

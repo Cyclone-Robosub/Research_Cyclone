56c40e39ec96c238b7cde0b295d17f65ae5b1b88
Tanishq Dwivedi <tanishq12.dwivedi@gmail.com>
2024-12-25 14:06:02 -0800

    forgot to update arduino code

--- main.ino
+++ main.ino
@@ -3,76 +3,68 @@
 #include <floatToString.h>
 #include <MS5837.h>
 MS5837 depthReader = MS5837();
-    TSYS01  tempReader = TSYS01();
-void startReading() {
-        String wantedData = "all";
-        if(depthReader.init())
-        {
-      depthReader.read();
-        }
-    if(tempReader.init()){
-      tempReader.read();
-    }
-    String tempStringC;
-        if(tempReader.temperature() < -1000)
-        {
-         // Serial.println("No Got it");
-           tempStringC = String(depthReader.temperature(),5); 
-        }else {
-         // Serial.println("Got it");
-          tempStringC = String(tempReader.temperature(),5);
-        }
-        String depthString = String(depthReader.depth(),5);
-        String altString = String(depthReader.altitude(),5);
-        String pressureString = String(depthReader.pressure(), 5);
-       
-        if(wantedData == "all"){
-         Serial.println(depthString+","+ altString +","+ tempStringC +"," +pressureString);
-        }
-        if(wantedData == "depth"){
-          Serial.println(depthReader.depth());
-        }
-        if(wantedData == "temp"){
-        //  Serial.println(tempReader.temperature());
-        }
-        if(wantedData == "ph"){
+TSYS01 tempReader = TSYS01();
+void startReading()
+{
+  if (depthReader.init())
+  {
+    depthReader.read();
+  }
+  if (tempReader.init())
+  {
+    tempReader.read();
+  }
+  String tempStringC;
+  if (tempReader.temperature() < -1000)
+  {
+    // Serial.println("No Got it");
+    tempStringC = String(depthReader.temperature(), 5);
+  }
+  else
+  {
+    // Serial.println("Got it");
+    tempStringC = String(tempReader.temperature(), 5);
+  }
+  String depthString = String(depthReader.depth(), 5);
+  String altString = String(depthReader.altitude(), 5);
+  String pressureString = String(depthReader.pressure(), 5);
 
-        }
-        if(wantedData == "altitude"){
-          Serial.println(depthReader.altitude());
-        }
-
-    
+  Serial.println("Reading: " + depthString + "," + altString + "," + tempStringC + "," + pressureString);
 }
 
-void startSensors() {
-    depthReader.setModel(MS5837::MS5837_30BA);
-    depthReader.setFluidDensity(997);
-    if(depthReader.init()){
-      Serial.println("Depth connected");
-    }
-    if(tempReader.init()){
-      Serial.println("Temperature connected");
-    }
-    
-    return 0;
+void startSensors()
+{
+  depthReader.setModel(MS5837::MS5837_30BA);
+  depthReader.setFluidDensity(997);
+  if (depthReader.init() && tempReader.init())
+  {
+    Serial.println("All sensors are ready.");
+  }
+  else
+  {
+    Serial.println("failed.")
+  }
+
+  return 0;
 }
-void setup() {
-    Serial.begin(9600);
-    Wire.begin();
-    startSensors();
-   // loop();
+void setup()
+{
+  Serial.begin(9600);
+  Wire.begin();
+  startSensors();
+  // loop();
 }
-void loop() {
-    String givenString = Serial.readStringUntil('.');
-    if(givenString == "start"){
-    while(givenString != "stop"){
+void loop()
+{
+  String givenString = Serial.readStringUntil('.');
+  if (givenString == "start")
+  {
+    while (givenString != "stop")
+    {
       givenString = Serial.readStringUntil('.');
       delay(10);
       startReading();
       givenString = Serial.readStringUntil('.');
     }
-    }
+  }
 }
-
-

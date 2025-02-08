b87bc17aa92c13de9dea352e2debb9dec15c159c
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-02-08 15:07:30 -0800

    commit ubuntu

--- main.py
+++ main.py
@@ -4,12 +4,10 @@ import serial
 import subprocess
 from pathlib import Path
 from datetime import datetime
-from Depth import DepthSens
 import io
 import string
-from Temperature import TempSensor
 
-    
+
 
 def main():
     start_input = input("Type y to start sensors.")
@@ -17,18 +15,16 @@ def main():
         if __name__ == '__main__':
             ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #Need to change the file path as needed.
             ser.reset_input_buffer()
-            subprocess.run(["g++","main.cpp" "StartandSend.cpp", "GetSensorData.cpp","-o","runprogram"])
+            subprocess.run(["g++ ","main.cpp " "StartandSend.cpp ",
+                            "GetSensorData.cpp ","-o ","runprogram"])
             run_path = subprocess.run(["./runprogram"], capture_output=True, text=True)
             if run_path.stdout == "failed":
                 print("Failed to start recording")
             else:
-                commandControl = input()
-                if commandControl == "All sensors are ready.":
-                    print("Started sensors")
-                    ser.write("start.")
-                else:
-                    print("failed to start sensors")
-                
+                ser.write("start.\n")
+                line = ser.readLine().decode('utf-8').rstrip()
+                print(line)
+
 
 
 

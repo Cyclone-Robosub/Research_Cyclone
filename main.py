ddc950ff25b2922d0cdb1a4cb03b8b344467bbac
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-02-08 15:18:59 -0800

    test again.

--- main.py
+++ main.py
@@ -10,15 +10,14 @@ import string
 
 
 def main():
-    start_input = input("Type y to start sensors.")
-    if start_input.upper() == "Y":
-        if __name__ == '__main__':
-            ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #Need to change the file path as needed.
-            ser.reset_input_buffer()
-            while True:
-                ser.write("start.\n")
-                line = ser.readLine().decode('utf-8').rstrip()
-                print(line)
+    if __name__ == '__main__':
+        ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
+        ser.reset_input_buffer()
+        ser.write(b"start.\n")
+        subprocess.run(["g++ ","main.cpp " "StartandSend.cpp ",
+                        "GetSensorData.cpp ","-o ","runprogram"])
+        run_path = subprocess.run(["./runprogram"], capture_output=True, text=True)
+                
 
 
 

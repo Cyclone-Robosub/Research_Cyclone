34c7bede81c1a47f56d4cc96ed288a75d867f443
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-02-08 15:11:52 -0800

    testing.

--- main.py
+++ main.py
@@ -15,12 +15,7 @@ def main():
         if __name__ == '__main__':
             ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1) #Need to change the file path as needed.
             ser.reset_input_buffer()
-            subprocess.run(["g++ ","main.cpp " "StartandSend.cpp ",
-                            "GetSensorData.cpp ","-o ","runprogram"])
-            run_path = subprocess.run(["./runprogram"], capture_output=True, text=True)
-            if run_path.stdout == "failed":
-                print("Failed to start recording")
-            else:
+            while True:
                 ser.write("start.\n")
                 line = ser.readLine().decode('utf-8').rstrip()
                 print(line)

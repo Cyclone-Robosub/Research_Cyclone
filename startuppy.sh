e930a5f2893582121c8fa50ae7ece44e651fbe87
Tanishq Dwivedi <tdwivedi@ucdavis.edu>
2025-04-18 05:11:00 +0000

    better run time

--- startuppy.sh
+++ startuppy.sh
@@ -1,7 +1,8 @@
 #Created By Tanishq Dwivedi
-
+source /opt/ros/jazzy/setup.bash
+source install/setup.bash
 cd src
 source /opt/ros/jazzy/setup.bash
 source install/setup.bash
 chmod +x startupcpp.sh
-python3 main.py
+python3 main.py &
